6f725d248d86991f680a9f3514fe0353abede9bb
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-15 14:33:19 +0000

    add #from_params to Post model

--- instagram/app/models/post.rb
+++ instagram/app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
+
+  class << self
+    def from_params(user_id, params)
+      user = User.find(user_id)
+      user.posts.create(params)
+    end
+  end
 end
