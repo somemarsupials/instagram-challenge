dcf30c460421558c6fa0517de6df87fe42e41126
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-15 09:56:12 +0000

    add whitelisting for ImageUploader

--- instagram/app/uploaders/image_uploader.rb
+++ instagram/app/uploaders/image_uploader.rb
@@ -12,6 +12,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -30,12 +34,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
   # end
 
   # Override the filename of the uploaded files:
