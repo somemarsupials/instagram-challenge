ae6be3cd9568eb2657855fd6ad4e6df4f534ca58
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-15 11:09:48 +0000

    add new and create routes to Post controller

--- instagram/app/controllers/posts_controller.rb
+++ instagram/app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+  def show
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to '/'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :image)
+  end
 end
