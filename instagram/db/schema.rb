ee27b7237c569f616ff0e14ee0d309e29a5abaeb
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-15 10:03:58 +0000

    migration adding title to Post

--- instagram/db/schema.rb
+++ instagram/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214182926) do
+ActiveRecord::Schema.define(version: 20171214185052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171214182926) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
